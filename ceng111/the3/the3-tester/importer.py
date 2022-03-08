76db7507ae48c3a32580aa2c98e5b8d85f181244
berk <berkulutas2001@gmail.com>
2022-03-08 19:56:42 +0300

    add ceng111

--- ceng111/the3/the3-tester/importer.py
+++
@@ -1,9 +0,0 @@
-import utils
-dataset = utils.load_dataset("mnist.pkl")
-network1 = utils.load_network("network_3layer.pkl")
-network2 = utils.load_network("network_2layer.pkl")
-
-X_sample = dataset['X_test'][100] # An image of digit 6
-utils.display_image(X_sample)
-from the3 import *
-forward_pass(network1, X_sample)
