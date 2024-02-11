f68ed970a0ac0a6329fac4284ed4c5f447be13d8
berkulutas <berkulutas2001@gmail.com>
2024-02-11 18:55:43 +0300

    added attacklab

---
+++ ceng331/attacklab/target48/farm.c
@@ -0,0 +1,131 @@
+/* This function marks the start of the farm */
+int start_farm()
+{
+    return 1;
+}
+
+void setval_474(unsigned *p)
+{
+    *p = 2425641283U;
+}
+
+unsigned addval_123(unsigned x)
+{
+    return x + 3284699464U;
+}
+
+unsigned getval_111()
+{
+    return 2445838664U;
+}
+
+void setval_278(unsigned *p)
+{
+    *p = 2446197064U;
+}
+
+void setval_132(unsigned *p)
+{
+    *p = 3284631880U;
+}
+
+unsigned getval_406()
+{
+    return 4203301116U;
+}
+
+void setval_492(unsigned *p)
+{
+    *p = 3599321275U;
+}
+
+void setval_495(unsigned *p)
+{
+    *p = 3254421832U;
+}
+
+unsigned getval_468()
+{
+    return 3284633928U;
+}
+
+unsigned addval_379(unsigned x)
+{
+    return x + 3281017276U;
+}
+
+unsigned addval_427(unsigned x)
+{
+    return x + 2429004104U;
+}
+
+void setval_154(unsigned *p)
+{
+    *p = 3351808328U;
+}
+
+void setval_187(unsigned *p)
+{
+    *p = 2429978952U;
+}
+
+unsigned addval_242(unsigned x)
+{
+    return x + 3281096793U;
+}
+
+void setval_375(unsigned *p)
+{
+    *p = 2432338248U;
+}
+
+void setval_125(unsigned *p)
+{
+    *p = 3285057864U;
+}
+
+unsigned getval_148()
+{
+    return 3284699464U;
+}
+
+unsigned addval_328(unsigned x)
+{
+    return x + 2425379291U;
+}
+
+unsigned addval_239(unsigned x)
+{
+    return x + 2428995912U;
+}
+
+unsigned getval_429()
+{
+    return 3254421832U;
+}
+
+unsigned addval_444(unsigned x)
+{
+    return x + 2462974280U;
+}
+
+void setval_279(unsigned *p)
+{
+    *p = 2429946184U;
+}
+
+unsigned addval_138(unsigned x)
+{
+    return x + 2429979464U;
+}
+
+unsigned getval_109()
+{
+    return 3456190609U;
+}
+
+/* This function marks the end of the farm */
+int end_farm()
+{
+    return 1;
+}
